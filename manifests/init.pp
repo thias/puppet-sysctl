145d2e930c4c822ee2b9dd523cfa6f4666e64164
Matthias Saou <matthias@saou.eu>
2013-03-06 20:33:58 +0100

    Use 2 space indent

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,34 +14,34 @@
 #
 define sysctl ( $value = undef, $ensure = undef ) {
 
-    # Parent purged directory
-    include sysctl::base
+  # Parent purged directory
+  include sysctl::base
 
-    # The permanent change
-    file { "/etc/sysctl.d/${title}.conf":
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => "${title} = ${value}\n",
-        ensure  => $ensure,
+  # The permanent change
+  file { "/etc/sysctl.d/${title}.conf":
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "${title} = ${value}\n",
+    ensure  => $ensure,
+  }
+
+  if $ensure != 'absent' {
+
+    # The immediate change + re-check on each run "just in case"
+    exec { "sysctl-${title}":
+      command => "/sbin/sysctl -w ${title}=${value}",
+      unless  => "/sbin/sysctl -n ${title} | /bin/grep -q -e '^${value}\$'",
     }
 
-    if $ensure != 'absent' {
-
-        # The immediate change + re-check on each run "just in case"
-        exec { "sysctl-${title}":
-            command => "/sbin/sysctl -w ${title}=${value}",
-            unless  => "/sbin/sysctl -n ${title} | /bin/grep -q -e '^${value}\$'",
-        }
-
-        # For the few original values from the main file
-        exec { "update-sysctl.conf-${title}":
-            command => "sed -i -e 's/${title} =.*/${title} = ${value}/' /etc/sysctl.conf",
-            unless  => "/bin/bash -c \"! egrep '^${title} =' /etc/sysctl.conf || egrep '^${title} = ${value}\$' /etc/sysctl.conf\"",
-            path    => [ '/usr/sbin', '/sbin', '/usr/bin', '/bin' ],
-        }
-
+    # For the few original values from the main file
+    exec { "update-sysctl.conf-${title}":
+      command => "sed -i -e 's/${title} =.*/${title} = ${value}/' /etc/sysctl.conf",
+      unless  => "/bin/bash -c \"! egrep '^${title} =' /etc/sysctl.conf || egrep '^${title} = ${value}\$' /etc/sysctl.conf\"",
+      path    => [ '/usr/sbin', '/sbin', '/usr/bin', '/bin' ],
     }
+
+  }
 
 }
 
