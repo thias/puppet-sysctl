40615fd3d8bdf725cb1774f4296598b3ad20fd51
Matthias Saou <matthias@saou.eu>
2014-09-05 13:03:33 +0200

    Start adding support for setting sysctl_dir to false

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,41 +4,46 @@
 #
 class sysctl::base (
   $purge              = false,
-  $symlink99          = $::sysctl::params::symlink99,
   $values             = undef,
   $hiera_merge_values = false,
+  $symlink99          = $::sysctl::params::symlink99,
+  $sysctl_dir         = $::sysctl::params::sysctl_dir,
+  $sysctl_dir_path    = $::sysctl::params::sysctl_dir_path,
+  $sysctl_dir_owner   = $::sysctl::params::sysctl_dir_owner,
+  $sysctl_dir_group   = $::sysctl::params::sysctl_dir_group,
+  $sysctl_dir_mode    = $::sysctl::params::sysctl_dir_mode,
 ) inherits ::sysctl::params {
 
+  # Hiera support
   if $hiera_merge_values == true {
     $values_real = hiera_hash('sysctl::base::values')
   } else {
     $values_real = $values
   }
-
   if $values_real != undef {
     create_resources(sysctl,$values_real)
   }
 
-  if $purge {
-    $recurse = true
-  } else {
-    $recurse = false
-  }
-
-  file { '/etc/sysctl.d':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    # Magic hidden here
-    purge   => $purge,
-    recurse => $recurse,
-  }
-
-  if $symlink99 {
-    file { '/etc/sysctl.d/99-sysctl.conf':
-      ensure => link,
-      target => '../sysctl.conf',
+  if $sysctl_dir {
+    if $purge {
+      $recurse = true
+    } else {
+      $recurse = false
+    }
+    file { $sysctl_dir_path:
+      ensure  => directory,
+      owner   => $sysctl_dir_owner,
+      group   => $sysctl_dir_group,
+      mode    => $sysctl_dir_mode,
+      # Magic hidden here
+      purge   => $purge,
+      recurse => $recurse,
+    }
+    if $symlink99 and $sysctl_dir_path =~ /^\/etc\/[^\/]+$/ {
+      file { "${sysctl_dir_path}/99-sysctl.conf":
+        ensure => link,
+        target => '../sysctl.conf',
+      }
     }
   }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -10,5 +10,18 @@ class sysctl::params {
     }
   }
 
+  case $::osfamily {
+    'FreeBSD': {
+      $sysctl_dir = false
+    }
+    default: {
+      $sysctl_dir = true
+      $sysctl_dir_path = '/etc/sysctl.d'
+      $sysctl_dir_owner = 'root'
+      $sysctl_dir_group = 'root'
+      $sysctl_dir_mode = '0755'
+    }
+  }
+
 }
 
