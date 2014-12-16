f0e496118b6ad2ed365db1c239152105b98ff3aa
Matthias Saou <matthias@saou.eu>
2014-12-16 13:46:35 +0100

    Release 1.0.1

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class sysctl::params {
 
   # Keep the original symlink if we purge, to avoid ping-pong with initscripts
-  case "${osfamily}-${operatingsystemmajrelease}" {
+  case "${::osfamily}-${::operatingsystemmajrelease}" {
     'RedHat-7': {
       $symlink99 = true
     }
