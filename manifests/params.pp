285a00e5831332b392281d39bd47f0b8cc730040
Marc Teinturier <marc.teinturier@dedietrichthermique.com>
2015-06-20 23:29:15 +0200

    Keep manualy symlink /etc/sysctl.d/99-sysctl.conf. Support for Debian 8. Documentation update

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class sysctl::params(
   else {
     # Keep the original symlink if we purge, to avoid ping-pong with initscripts
     case "${::osfamily}-${::operatingsystemmajrelease}" {
-      'RedHat-7', 'Debian-9': {
+      'RedHat-7', 'Debian-8': {
         $symlink99 = true
       }
       default: {
