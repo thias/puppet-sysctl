2a0c0f5e9d6ab7b1801a54bd3bce681f04ff1b1b
Julien Kassar <github@kassisol.com>
2016-04-08 11:46:23 -0400

    Add parsing of value provided for enforce

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,7 +90,7 @@ define sysctl (
       $qvalue = shellquote("${value}")
       # lint:endignore
       exec { "enforce-sysctl-value-${qtitle}":
-          unless  => "/usr/bin/test \"$(/sbin/sysctl -n ${qtitle} | sed -r 's/\\s+/ /g')\" = ${qvalue}",
+          unless  => "/usr/bin/test \"$(/sbin/sysctl -n ${qtitle} | sed -r 's/\\s+/ /g')\" = \"$(/bin/echo ${qvalue} | sed -r 's/\\s+/ /g')\"",
           command => "/sbin/sysctl -w ${qtitle}=${qvalue}",
       }
     }
