b097c1fa217833472e553e7543b99c2af51881cc
Phil Friderici <phil.friderici@elits.com>
2023-08-08 12:38:14 +0000

    Use ternary operator for $symlink99 instead of if/else

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,6 @@
 # @api private
 class sysctl::params {
   # Keep the original symlink if we purge, to avoid ping-pong with initscripts
-  if (
-    $facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '7') >= 0
-  ) or (
-    $facts['os']['family'] == 'Debian' and versioncmp($facts['os']['release']['major'], '8') >= 0
-  ) {
-    $symlink99 = true
-  } else {
-    $symlink99 = false
-  }
+  $symlink99 = ($facts['os']['family'] == 'RedHat' and versioncmp($facts['os']['release']['major'], '7') >= 0) or
+  ($facts['os']['family'] == 'Debian' and versioncmp($facts['os']['release']['major'], '8') >= 0)
 }
