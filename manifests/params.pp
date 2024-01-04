2f39e59dc9908d05a07862ea4ed0853db62f78d7
Spindyckx <hindryckxrobin@gmail.com>
2024-01-04 14:14:32 +0100

    Revert "puppet-lint & write some documentation about the parameters"
    This reverts commit afe292fbb9e973845e161f1ea5c03267ad9dc088.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,15 @@
-# parameters for systcl
 class sysctl::params {
   # Keep the original symlink if we purge, to avoid ping-pong with initscripts
-  if ($facts['os']['family'] == 'RedHat' and
-  versioncmp($facts['os']['release']['major'], '7') >= 0) or
-  ($facts['os']['family'] == 'Debian' and
-  versioncmp($facts['os']['release']['major'], '8') >= 0) {
+  if ($::osfamily == 'RedHat' and
+      versioncmp($::operatingsystemmajrelease, '7') >= 0) or
+    ($::osfamily == 'Debian' and
+      versioncmp($::operatingsystemmajrelease, '8') >= 0) {
     $symlink99 = true
   } else {
     $symlink99 = false
   }
 
-  case $facts['os']['family'] {
+  case $::osfamily {
     'FreeBSD': {
       $sysctl_dir = false
     }
